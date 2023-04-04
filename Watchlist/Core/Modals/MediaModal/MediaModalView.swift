9ede18400cf904f7620846c993519a16afc1a5e1
TJ Goldblatt <tj.goldblatt@wolterskluwer.com>
2023-04-04 08:34:48 -0400

    refactoring media modal view to have its own view model

--- Watchlist/Core/Modals/MediaModal/MediaModalView.swift
+++ Watchlist/Core/Modals/MediaModal/MediaModalView.swift
@@ -14,28 +14,12 @@ struct MediaModalView: View {
     
     @EnvironmentObject var homeVM: HomeViewModel
     
+    @StateObject var vm: MediaModalViewModel
+    
     @BlackbirdLiveModels({ try await MediaModel.read(from: $0) }) var mediaList
     
-    @State var mediaDetails: MediaDetailContents
-    @State var media: Media
-    
-    @State var personalRating: Double?
-    
-    @State var isAdded = false
-    @State var isWatched = false
-    
-    @State private var showingRating = false
-    @State private var showDeleConfirmation = false
-    
-    @State private var selectedOption: String = "Clear Rating"
-    let options = ["Clear Rating"]
-    
-    var imagePath: String {
-        if let backdropPath = mediaDetails.backdropPath {
-            return backdropPath
-        } else {
-            return mediaDetails.posterPath
-        }
+    init(mediaDetails: MediaDetailContents, media: Media) {
+     _vm = StateObject(wrappedValue: MediaModalViewModel(mediaDetails: mediaDetails, media: media))
     }
     
     var body: some View {
@@ -66,16 +50,16 @@ struct MediaModalView: View {
             }
         }
         .overlay(alignment: .topTrailing) {
-            if isInMedia(mediaModels: mediaList.results, media: media) && isWatched {
+            if isInMedia(mediaModels: mediaList.results, media: vm.media) && vm.isWatched {
                 Menu {
                     Button(role: .destructive) {
                         Task {
-                            await database?.sendRating(rating: nil, media: media)
-                            await database?.fetchPersonalRating(media: media) { rating in
-                                personalRating = rating
+                            await database?.sendRating(rating: nil, media: vm.media)
+                            await database?.fetchPersonalRating(media: vm.media) { rating in
+                                vm.personalRating = rating
                             }
-                            await database?.setWatched(watched: false, media: media)
-                            isWatched = false
+                            await database?.setWatched(watched: false, media: vm.media)
+                            vm.isWatched = false
                         }
                     } label: {
                         Text("Reset")
@@ -95,12 +79,12 @@ struct MediaModalView: View {
         }
         .onAppear {
             Task {
-                await database?.fetchPersonalRating(media: media) { rating in
-                    personalRating = rating
+                await database?.fetchPersonalRating(media: vm.media) { rating in
+                    vm.personalRating = rating
                 }
                 
-                await database?.fetchIsWatched(media: media, completionHandler: { watched in
-                    isWatched = watched
+                await database?.fetchIsWatched(media: vm.media, completionHandler: { watched in
+                    vm.isWatched = watched
                 })
             }
         }
@@ -116,7 +100,7 @@ struct MediaDetailView_Previews: PreviewProvider {
 
 extension MediaModalView {
     private var backdropSection: some View {
-        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(imagePath)")) { image in
+        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/original\(vm.imagePath)")) { image in
             image
                 .resizable()
                 .scaledToFill()
@@ -131,7 +115,7 @@ extension MediaModalView {
     
     private var genreSection: some View {
         HStack {
-            if let genres = mediaDetails.genres {
+            if let genres = vm.mediaDetails.genres {
                 GenreSection(genres: genres)
             } else {
                 Spacer()
@@ -142,13 +126,13 @@ extension MediaModalView {
     private var titleSection: some View {
         VStack(alignment: .leading, spacing: 10) {
             HStack {
-                Text(mediaDetails.title)
+                Text(vm.mediaDetails.title)
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                     .foregroundColor(Color.theme.text)
                     .multilineTextAlignment(.leading)
                 
-                if isWatched {
+                if vm.isWatched {
                     Image(systemName: "checkmark.circle.fill")
                         .foregroundColor(Color.theme.red)
                         .imageScale(.large)
@@ -160,7 +144,7 @@ extension MediaModalView {
     }
     
     private var overview: some View {
-        ExpandableText(text: mediaDetails.overview, lineLimit: 3)
+        ExpandableText(text: vm.mediaDetails.overview, lineLimit: 3)
     }
     
     private var ratingSection: some View {
@@ -169,15 +153,15 @@ extension MediaModalView {
             
             Spacer()
             
-            StarRatingView(text: "IMDb RATING", rating: mediaDetails.imdbRating, size: 18)
+            StarRatingView(text: "IMDb RATING", rating: vm.mediaDetails.imdbRating, size: 18)
             
             Spacer()
             
-            if let personalRating {
+            if let personalRating = vm.personalRating {
                 StarRatingView(text: "PERSONAL RATING", rating: personalRating, size: 18)
             } else {
                 rateThisButton
-                    .disabled(isInMedia(mediaModels: mediaList.results, media: media) ? false : true)
+                    .disabled(isInMedia(mediaModels: mediaList.results, media: vm.media) ? false : true)
             }
         }
         .padding(.trailing)
@@ -186,59 +170,59 @@ extension MediaModalView {
     private var rateThisButton: some View {
         Button {
             homeVM.hapticFeedback.impactOccurred()
-            showingRating.toggle()
+            vm.showingRating.toggle()
         } label: {
             VStack {
                 Image(systemName: "star")
                     .font(.system(size: 18))
                     .fontWeight(.bold)
-                    .foregroundColor(isInMedia(mediaModels: mediaList.results, media: media) ? Color.theme.red : Color.theme.secondary)
+                    .foregroundColor(isInMedia(mediaModels: mediaList.results, media: vm.media) ? Color.theme.red : Color.theme.secondary)
                 Text("Rate This")
                     .font(.subheadline)
                     .fontWeight(.semibold)
-                    .foregroundColor(isInMedia(mediaModels: mediaList.results, media: media) ? Color.theme.red : Color.theme.secondary)
+                    .foregroundColor(isInMedia(mediaModels: mediaList.results, media: vm.media) ? Color.theme.red : Color.theme.secondary)
             }
         }
-        .sheet(isPresented: $showingRating, onDismiss: {
+        .sheet(isPresented: $vm.showingRating, onDismiss: {
             Task {
-                await database?.fetchPersonalRating(media: media) { rating in
-                    personalRating = rating
+                await database?.fetchPersonalRating(media: vm.media) { rating in
+                    vm.personalRating = rating
                 }
-                if personalRating != nil {
-                    await database?.setWatched(watched: true, media: media)
-                    await database?.fetchIsWatched(media: media) { watched in
-                        isWatched = watched
+                if vm.personalRating != nil {
+                    await database?.setWatched(watched: true, media: vm.media)
+                    await database?.fetchIsWatched(media: vm.media) { watched in
+                        vm.isWatched = watched
                     }
                 }
             }
         }) {
-            RatingModalView(media: media)
+            RatingModalView(media: vm.media)
         }
     }
     
     private var addButton: some View {
         Button {
             homeVM.hapticFeedback.impactOccurred()
-            if !isInMedia(mediaModels: mediaList.results, media: media) {
-                database?.saveMedia(media: media)
+            if !isInMedia(mediaModels: mediaList.results, media: vm.media) {
+                database?.saveMedia(media: vm.media)
             } else {
-                showDeleConfirmation.toggle()
+                vm.showDeleteConfirmation.toggle()
             }
         } label: {
-            Text(!isInMedia(mediaModels: mediaList.results, media: media) ? "Add" : "Added")
+            Text(!isInMedia(mediaModels: mediaList.results, media: vm.media) ? "Add" : "Added")
                 .font(.system(size: 18))
                 .fontWeight(.medium)
-                .foregroundColor(!isInMedia(mediaModels: mediaList.results, media: media) ? Color.theme.red : Color.theme.genreText)
+                .foregroundColor(!isInMedia(mediaModels: mediaList.results, media: vm.media) ? Color.theme.red : Color.theme.genreText)
                 .padding(.vertical, 10)
                 .padding(.horizontal)
                 .background {
                     RoundedRectangle(cornerRadius: 5)
-                        .foregroundColor(!isInMedia(mediaModels: mediaList.results, media: media) ? Color.theme.secondary : Color.theme.red)
+                        .foregroundColor(!isInMedia(mediaModels: mediaList.results, media: vm.media) ? Color.theme.secondary : Color.theme.red)
                         .frame(width: 80, height: 30)
                 }
         }
-        .alert("Are you sure you'd like to delete from your Watchlist?", isPresented: $showDeleConfirmation, actions: {
-            Button("Delete", role: .destructive) { database?.deleteMedia(media: media) }
+        .alert("Are you sure you'd like to delete from your Watchlist?", isPresented: $vm.showDeleteConfirmation, actions: {
+            Button("Delete", role: .destructive) { database?.deleteMedia(media: vm.media) }
             Button("Cancel", role: .cancel) {}
         })
         .frame(width: 100, alignment: .center)
@@ -283,6 +267,11 @@ struct ExpandableText: View {
             Text(text)
                 .lineLimit(isExpanded ? nil : lineLimit)
                 .background(calculateTruncation(text: text))
+                .onTapGesture {
+                    withAnimation(.interactiveSpring()) {
+                        isExpanded = true
+                    }
+                }
             
             if isTruncated == true {
                 button
