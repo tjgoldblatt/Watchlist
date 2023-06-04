bf7dd5ae05173949f82cf3eab5e2cc00e4e53799
TJ Goldblatt <tj@psyton.com>
2023-06-04 19:48:24 -0400

    hoping to fix xcode cloud build errors

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  Watchlist
+#
+#  Created by TJ Goldblatt on 6/4/23.
+#  
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
