a7639d5556822451efb612ec352a2c0e611acaab
TJ Goldblatt <tj@psyton.com>
2023-07-12 09:24:27 -0400

    Testing build versioning

--- scripts/set_build_number_by_git_commits.sh
+++ scripts/set_build_number_by_git_commits.sh
@@ -17,7 +17,7 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
+#    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
 #    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
   fi
 done
