824ea88f5fdd1bd56578917a427d1fb1e17522fe
TJ Goldblatt <tj@psyton.com>
2023-07-12 09:33:56 -0400

    build # stuff

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,8 +4,16 @@
 #  Watchlist
 #
 #  Created by TJ Goldblatt on 6/4/23.
-#  
+#
+
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
-echo "Starting build number"
-buildNumber=$(CI_BUILD_NUMBER)
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$plist"
+
+buildNumber=$CI_BUILD_NUMBER
+target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
+dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
+
+for plist in "$target_plist" "$dsym_plist"; do
+    if [ -f "$plist" ]; then
+        /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$plist"
+    fi
+done
